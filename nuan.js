6c446a95e480bf27c7cd352c6bf812cf86e47729
ife <yorkisraining@users.noreply.github.com>
2017-10-16 09:57:33 -0500

    Update nuan.js

--- nuan.js
+++ nuan.js
@@ -1,17 +1,17 @@
 var iTop = 50,
 	iLeft = -280;
-var wrap = document.getElementsByClassName('qi-bigpic')[0],
-	people = document.getElementsByClassName('qi-bigpic-people')[0];
+
+var people = document.getElementsByClassName('qi-bigpic-people')[0];
 
 var swidth = document.documentElement.clientWidth,
 	sheight = document.documentElement.clientHeight;
 
 var perx = (1920 - swidth) / (swidth * 5),
-	pery = sheight / (766 * 40);
+	pery = sheight / (766 * 20);
 
 var vx, vy, nowX, nowY, shouldx, shouldy;
 
-wrap.addEventListener('mousemove', function(e) {
+window.addEventListener('mousemove', function(e) {
 
 
 	nowX = e.clientX;
@@ -38,3 +38,37 @@ wrap.addEventListener('mousemove', function(e) {
 	people.style.left = iLeft + 'px';
 
 });
+
+
+
+// 新闻模块轮播
+var pic = document.getElementsByClassName('roll-pic')[0];
+var picCount = 0;
+var picNum = 3
+var picWidth = 525;
+setInterval(function() {
+	pic.style.left = picCount * -picWidth + 'px';
+	if (picCount < (picNum-1)) {
+		picCount++;
+	} else {
+		picCount = 0;
+	}
+}, 5000);
+
+// 游戏特色轮播
+var feature = document.getElementsByClassName('feature-swiper-wrap')[0];
+var featureCount = 0;
+var featureNum = 5;
+var featureWidth = 1200;
+setInterval(function() {
+	feature.style.left = featureCount * -featureWidth + 'px';
+	if (featureCount < (featureNum-1)) {
+		featureCount++;
+	} else {
+		featureCount = 0;
+	}
+}, 5000);
+
+
+
+
